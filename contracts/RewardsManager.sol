ce8755ee75d11beb12cb51c3bd3f2aa25db5ba8d
princetonbishop <princetonbishop@protonmail.com>
2022-04-25 17:01:40 +0200

    reward manager supporting multiple rewards

--- contracts/RewardsManager.sol
+++ contracts/RewardsManager.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IStaxStaking {
-    function notifyRewardAmount(uint256 reward) external;
+    function notifyRewardAmount(address token, uint256 reward) external;
     function periodFinish() external view returns (uint256);
 }
 
@@ -20,9 +20,12 @@ contract RewardsManager is Ownable {
     using SafeERC20 for IERC20;
 
     address public staking;
-    address public rewardToken; // main reward token for staking
+    //address public rewardToken; // main reward token for staking
     address public operator;
     address public lpLocker;
+    
+    address[] public rewardTokens;
+    mapping(address => bool) public isRewardToken; // to check if token is reward token as distribution of tokens don't happen at the same time
 
     event RewardDistributed(address staking, address token, uint256 amount);
     event ExtraRewardTokenTransferred(address to, address token, uint256 amount);
@@ -30,12 +33,16 @@ contract RewardsManager is Ownable {
     constructor(
         address _staking,
         address _lpLocker,
-        address _rewardToken
+        address[] memory _rewardTokens
     ) {
         staking = _staking;
-        rewardToken = _rewardToken;
         lpLocker = _lpLocker;
         operator = msg.sender;
+
+        for (uint i=0; i<_rewardTokens.length; i++) {
+            rewardTokens.push(_rewardTokens[i]);
+            isRewardToken[_rewardTokens[i]] = true;
+        }
     }
 
     function setOperator(address _operator) external onlyOwner {
@@ -49,9 +56,9 @@ contract RewardsManager is Ownable {
     function harvest(bool _shouldDistribute) external onlyOperator {
         // @dev there's no notion of harvest interval here so operator is assumed to know when to harvest
         ILPLocker(lpLocker).harvestRewards();
-        if (_shouldDistribute) {
-            _distribute();
-        }
+        //if (_shouldDistribute) {
+        //    _distribute();
+        //}
     }
 
     function swapTokenForxLP(address _token, bool _burnTokens) external onlyOperator {
@@ -59,7 +66,7 @@ contract RewardsManager is Ownable {
     }
 
     // swap claimed rewards to temple
-    function swapFXSToRewardToken(bool _shouldDistribute, address[] calldata _targets, bytes[] calldata _data) external onlyOperator {
+    /*function swapFXSToRewardToken(bool _shouldDistribute, address[] calldata _targets, bytes[] calldata _data) external onlyOperator {
         bool success;
         for (uint i=0; i<_targets.length; i++) {
             (success,) = _targets[i].call{value:0}(_data[i]);
@@ -68,20 +75,21 @@ contract RewardsManager is Ownable {
         if (_shouldDistribute) {
             _distribute();
         }
+    }*/
+
+    function distribute(address _token) external onlyOperator {
+        require(isRewardToken[_token], "not reward token");
+        _distribute(_token);
     }
 
-    function distribute() external onlyOperator {
-        _distribute();
-    }
-
-    function _distribute() internal {
+    function _distribute(address _token) internal {
         require(block.timestamp > IStaxStaking(staking).periodFinish(), "last reward duration not finished");
-        _notifyRewardsAmount(IERC20(rewardToken).balanceOf(address(this)));
+        _notifyRewardsAmount(_token, IERC20(_token).balanceOf(address(this)));
     }
 
     // @dev transfer extra reward token to extra reward contract in the event rewards were previously liquidated into another token
     function transferExtraRewardToken(address _token, address _to) external onlyOperator {
-        require(_token != rewardToken, "not extra reward");
+        require(!isRewardToken[_token], "not extra reward");
         uint256 amount = IERC20(_token).balanceOf(address(this));
         IERC20(_token).safeTransfer(_to, amount);
 
@@ -89,11 +97,11 @@ contract RewardsManager is Ownable {
     }
 
     // @dev notify staking contract about new rewards
-    function _notifyRewardsAmount(uint256 _amount) internal {
-        IERC20(rewardToken).safeIncreaseAllowance(staking, _amount);
-        IStaxStaking(staking).notifyRewardAmount(_amount);
+    function _notifyRewardsAmount(address _token, uint256 _amount) internal {
+        IERC20(_token).safeIncreaseAllowance(staking, _amount);
+        IStaxStaking(staking).notifyRewardAmount(_token, _amount);
 
-        emit RewardDistributed(staking, rewardToken, _amount);
+        emit RewardDistributed(staking, _token, _amount);
     }
 
 
