29be3ee93d13b6d35975a9ac2af8da2a61b55ec2
princetonbishop <princetonbishop@protonmail.com>
2022-04-30 16:35:12 +0200

    rewards manager stub

---
+++ test/rewards-manager.ts
@@ -0,0 +1,23 @@
+import { ethers, network } from "hardhat";
+import { Signer } from "ethers";
+import { expect } from "chai";
+import { shouldThrow } from "./helpers";
+
+
+describe("Rewards Manager", async () => {
+    let owner: Signer
+    let alan: Signer
+
+    beforeEach(async () => {
+
+
+    });
+
+    it("admin tests", async () => {
+
+    });
+
+    it("can distribute", async () => {
+
+    });
+});
