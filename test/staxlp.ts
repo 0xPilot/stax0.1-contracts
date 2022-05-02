d4b8040f03ffc22290ebf08c642da2f99d1e9143
0xPilot <pilot.majors@gmail.com>
2022-05-02 16:41:26 +1000

    stax lp tests

--- test/staxlp.ts
+++ test/staxlp.ts
@@ -1,12 +1,10 @@
-import { ethers, network } from "hardhat";
+import { ethers } from "hardhat";
 import { Signer } from "ethers";
 import { expect } from "chai";
 import { shouldThrow } from "./helpers";
 import { StaxLP, StaxLP__factory } from "../typechain";
 
-
-
-describe("Stax LP Token", async () => {
+describe.only("Stax LP Token", async () => {
     let staxToken: StaxLP;
     let owner: Signer
     let minter: Signer
@@ -15,15 +13,42 @@ describe("Stax LP Token", async () => {
     beforeEach(async () => {
         [owner, minter, alan] = await ethers.getSigners();
         staxToken = await new StaxLP__factory(owner).deploy("Stax LP Token", "xLP");
-
-        await staxToken.addMinter(await minter.getAddress());
     });
 
     it("Only specified roles can mint", async () => {
-
+        const alanAddress: string = await alan.getAddress();
+        const minterAddress: string = await minter.getAddress();
+    
+        // mint should fail when no minter set.
+        await shouldThrow(staxToken.mint(alanAddress, 10), /Caller cannot mint/);
+    
+        // Only admin can add a minter
+        await shouldThrow(staxToken.connect(alan).addMinter(alanAddress), /Ownable: caller is not the owner/);
+        await staxToken.addMinter(minterAddress);
+    
+        // Only minter can, well mint
+        await staxToken.connect(minter).mint(alanAddress, 10);
+        expect(await staxToken.balanceOf(alanAddress)).equals(10);
+        await shouldThrow(staxToken.mint(alanAddress, 10), /Caller cannot mint/);
+    
+        // Only admin can remove a minter
+        await shouldThrow(staxToken.connect(alan).removeMinter(minterAddress), /Ownable: caller is not the owner/);
+        await staxToken.removeMinter(minterAddress);
     });
 
     it("only specified roles can burn", async () => {
-
+        const alanAddress: string = await alan.getAddress();
+        const minterAddress: string = await minter.getAddress();
+    
+        // mint should fail when no minter set.
+        await shouldThrow(staxToken.burn(alanAddress, 10), /Caller cannot burn/);
+    
+        await staxToken.addMinter(minterAddress);
+    
+        // Only minter can burn
+        await staxToken.connect(minter).mint(alanAddress, 100);
+        await staxToken.connect(minter).burn(alanAddress, 10);
+        expect(await staxToken.balanceOf(alanAddress)).equals(90);
+        await shouldThrow(staxToken.burn(alanAddress, 10), /Caller cannot burn/);
     });
 });
